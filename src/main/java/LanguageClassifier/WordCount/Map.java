42f6f97bac4aa0a1ed72a0ebd1b62ade868dbf9b
Thomas Korevaar <thomaskorevaar.dev@gmail.com>
2021-05-02 13:49:11 +0200

    Rough draft for bigram generation

--- src/main/java/LanguageClassifier/WordCount/Map.java
+++ src/main/java/LanguageClassifier/WordCount/Map.java
@@ -11,16 +11,54 @@ import org.apache.hadoop.mapreduce.Mapper;
 public class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
 	private final static IntWritable one = new IntWritable(1);
 	private static final Pattern WORD_BOUNDARY = Pattern.compile("\\s*\\b\\s*");
-
-	public void map(LongWritable offset, Text lineText, Context context) throws IOException, InterruptedException {
+	
+	private String CreateStringFromChar(char... args)
+	{
+		//Create empty string object
+		String result = "";
+		
+		//Concatenate string from characters
+		for (char c : args)
+		{
+			//0 is Null in ASCII
+			result += (c == 0 ? "" : c);
+		}
+		
+		return result;
+	}
+	
+	public void map(LongWritable offset, Text lineText, Context context) throws IOException, InterruptedException 
+	{
+		//Get current line as String 
 		String line = lineText.toString();
-		Text currentWord = new Text();
-		for (String word : WORD_BOUNDARY.split(line)) {
-			if (word.isEmpty()) {
+		
+		//Loop over individual words in string
+		for (String word : WORD_BOUNDARY.split(line)) 
+		{
+			//Empty check
+			if (word.isEmpty()) 
+			{
 				continue;
 			}
-			currentWord = new Text(word);
-			context.write(currentWord, one);
+			
+			//Variable to hold previous character in word
+			char previousCharacter = 0;
+			
+			//Loop over individual characters in word
+			for (char currentCharacter : word.toCharArray())
+			{
+				//Create writable string from characters
+				String writable = CreateStringFromChar(previousCharacter, currentCharacter);
+				
+				//If writable is not empty and not a single character, write to output
+				if (!writable.isEmpty() && writable.length() > 1)
+				{
+					context.write(new Text(writable), one);
+				}
+				
+				//Set current character to previous for use in next iteration
+				previousCharacter = currentCharacter;
+			}
 		}
 	}
 }
