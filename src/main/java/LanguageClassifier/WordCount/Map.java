32a8f3c96aae5add1454958ea3154da85b694b30
Thomas Korevaar <thomaskorevaar.dev@gmail.com>
2021-05-15 15:39:02 +0200

    Maximum entropy changes

--- src/main/java/LanguageClassifier/WordCount/Map.java
+++ src/main/java/LanguageClassifier/WordCount/Map.java
@@ -11,10 +11,18 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
 public class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
-	private static final IntWritable one = new IntWritable(1);
+	// The '1' digit
+	private static final IntWritable ONE = new IntWritable(1);
 
+	// Matching pattern for sentences
 	private static final Pattern SENTENCE_BOUNDARY = Pattern.compile("(.*)\\.?");
 
+	// Constants for output
+	private static final String DUTCH = "Dutch";
+	private static final String ENGLISH = "ENGLISH";
+	private static final String SENTENCE_RESULT = "Current sentence: %s\nLanguage: %s\n";
+
+	// Helper function to get sentences from slab of text
 	public ArrayList<String> getSentenceFromText(String text) {
 		ArrayList<String> result = new ArrayList<String>();
 		Matcher m = SENTENCE_BOUNDARY.matcher(text);
@@ -32,9 +40,15 @@ public class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
 		return result;
 	}
 
+	// Mapper function that takes text files from input
 	public void map(LongWritable offset, Text lineText, Context context) throws IOException, InterruptedException {
+		// Get current file name
 		String currentFile = ((FileSplit) context.getInputSplit()).getPath().getName();
+		// Current text
 		String text = lineText.toString();
+
+		// Get all sentences from the text as an array and add it to the matrix
+		// Matrixes are stored in the "main" for use throughout the application
 
 		// Add to dutch word matrix
 		if (currentFile.equals("dutch.txt")) {
@@ -46,6 +60,7 @@ public class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
 			WordCount.englishMatrix.addToMatrix(getSentenceFromText(text));
 		}
 
+		// The test matrix is what we compare against using our trained matrixes
 		if (currentFile.equals("test.txt")) {
 			ArrayList<String> sentences = getSentenceFromText(text);
 			WordCount.testMatrix.addToMatrix(sentences);
@@ -55,9 +70,11 @@ public class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
 				double englishScore = WordCount.englishMatrix.testSentence(sentence, WordCount.testMatrix);
 
 				if (dutchScore > englishScore) {
-					context.write(new Text("Dutch"), one);
+					context.write(new Text(String.format(SENTENCE_RESULT, sentence, DUTCH)), ONE);
+					context.write(new Text(DUTCH), ONE);
 				} else if (dutchScore < englishScore) {
-					context.write(new Text("English"), one);
+					context.write(new Text(String.format(SENTENCE_RESULT, sentence, ENGLISH)), ONE);
+					context.write(new Text(ENGLISH), ONE);
 				}
 			}
 		}
